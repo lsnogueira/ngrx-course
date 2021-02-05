3ee45a664920fb2d3560de19127e33c27c88973d
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-02-05 18:11:22 -0300

    apply ngrx effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ import {
 } from '@angular/router';
 import { AppState } from './reducers';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
-import { logout } from './auth/auth.actions';
+import { login, logout } from './auth/auth.actions';
+import { AuthActions } from './auth/action-types';
 
 @Component({
   selector: 'app-root',
@@ -27,6 +28,12 @@ export class AppComponent implements OnInit {
   constructor(private router: Router, private store: Store<AppState>) {}
 
   ngOnInit() {
+    const userProfile = localStorage.getItem('user');
+
+    if (userProfile) {
+      this.store.dispatch(login({ user: JSON.parse(userProfile) }));
+    }
+
     this.router.events.subscribe((event) => {
       switch (true) {
         case event instanceof NavigationStart: {
