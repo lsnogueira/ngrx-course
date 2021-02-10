34a758fb1f2c296b4a6e6e0f792eee48c12daabb
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-02-10 10:32:05 -0300

    Revert "Merge branch '1-start'"
    This reverts commit b4c8d6b0d0835333373929667a97bf1e762b7cc4, reversing
    changes made to 6d38727072d3a0f1ba695ae6ddd1d912bfe72adf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
-import {
-  NavigationCancel,
-  NavigationEnd,
-  NavigationError,
-  NavigationStart,
-  Router,
-} from '@angular/router';
-import { AppState } from './reducers';
-import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
-import { login, logout } from './auth/auth.actions';
+import {Component, OnInit} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {distinctUntilChanged, map} from 'rxjs/operators';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import {AppState} from './reducers';
+import {isLoggedIn, isLoggedOut} from './auth/auth.selectors';
+import {login, logout} from './auth/auth.actions';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loading = true;
 
-  isLoggedIn$: Observable<boolean>;
-  isLoggedOut$: Observable<boolean>;
+    loading = true;
 
-  constructor(private router: Router, private store: Store<AppState>) {}
+    isLoggedIn$: Observable<boolean>;
 
-  ngOnInit() {
-    const userProfile = localStorage.getItem('user');
+    isLoggedOut$: Observable<boolean>;
 
-    if (userProfile) {
-      this.store.dispatch(login({ user: JSON.parse(userProfile) }));
+    constructor(private router: Router,
+                private store: Store<AppState>) {
+
     }
 
-    this.router.events.subscribe((event) => {
-      switch (true) {
-        case event instanceof NavigationStart: {
-          this.loading = true;
-          break;
+    ngOnInit() {
+
+        const userProfile = localStorage.getItem("user");
+
+        if (userProfile) {
+            this.store.dispatch(login({user: JSON.parse(userProfile)}));
         }
 
-        case event instanceof NavigationEnd:
-        case event instanceof NavigationCancel:
-        case event instanceof NavigationError: {
-          this.loading = false;
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    });
+        this.router.events.subscribe(event => {
+            switch (true) {
+                case event instanceof NavigationStart: {
+                    this.loading = true;
+                    break;
+                }
 
-    this.isLoggedIn$ = this.store.pipe(select(isLoggedIn));
+                case event instanceof NavigationEnd:
+                case event instanceof NavigationCancel:
+                case event instanceof NavigationError: {
+                    this.loading = false;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        });
 
-    this.isLoggedOut$ = this.store.pipe(select(isLoggedOut));
+        this.isLoggedIn$ = this.store
+            .pipe(
+                select(isLoggedIn)
+            );
 
-    this.store.dispatch(logout());
-  }
+        this.isLoggedOut$ = this.store
+            .pipe(
+                select(isLoggedOut)
+            );
 
-  logout() {
-    this.store.dispatch(logout());
-  }
+    }
+
+    logout() {
+
+        this.store.dispatch(logout());
+
+    }
+
 }
